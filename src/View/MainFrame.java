307e75795a4c7df63777f5c602c9c2d4a9df5276
junysz <daniel.junyszek@mycit.ie>
2014-02-20 14:47:14 +0000

    TabbedPaneAdded

--- src/View/MainFrame.java
+++ src/View/MainFrame.java
@@ -4,6 +4,10 @@ import java.awt.BorderLayout;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import javax.swing.JTabbedPane;
 
 public class MainFrame extends JFrame {
 	private static final long serialVersionUID = -7100419815175830650L;
@@ -16,10 +20,22 @@ public class MainFrame extends JFrame {
 	public MainFrame() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
+		
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		
+		JMenu mnFile = new JMenu("File");
+		menuBar.add(mnFile);
+		
+		JMenuItem mntmView = new JMenuItem("View");
+		menuBar.add(mntmView);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		
+		TabsPane tabbedPane = new TabsPane();
+		contentPane.add(tabbedPane, BorderLayout.CENTER);
 	}
 
 }
---
+++ src/View/TabsPane.java
@@ -0,0 +1,15 @@
+package View;
+
+import javax.swing.JTabbedPane;
+
+public class TabsPane extends JTabbedPane {
+	private static final long serialVersionUID = -1093584118198122580L;
+
+	/**
+	 * Create the panel.
+	 */
+	public TabsPane() {
+
+	}
+
+}
