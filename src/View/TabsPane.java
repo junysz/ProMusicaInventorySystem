001cc170b5b4f4c3fa8183005c868f9faf8e05f3
junysz <daniel.junyszek@mycit.ie>
2014-02-21 23:02:21 +0000

    Info added to tabbed pane class
    The link in the class is the tutorial from oracle on how to use tabbed
    pane J.I.C.

--- src/View/TabsPane.java
+++ src/View/TabsPane.java
@@ -1,7 +1,11 @@
 package View;
 
 import javax.swing.JTabbedPane;
-
+/*
+ * 
+ *  RELEVANT INFORMATION ON HOW TO USE TABBED PANES IN THE LINK BELOW:
+ *   http://docs.oracle.com/javase/tutorial/uiswing/components/tabbedpane.html
+ */
 public class TabsPane extends JTabbedPane {
 	private static final long serialVersionUID = -1093584118198122580L;
 
