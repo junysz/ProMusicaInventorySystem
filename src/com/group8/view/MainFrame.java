68ab1aecaeeaa5f3b486952598f0d8e59f293ecc
junysz <daniel.junyszek@mycit.ie>
2014-04-13 21:33:52 +0100

    menu deleted

--- src/com/group8/view/MainFrame.java
+++ src/com/group8/view/MainFrame.java
@@ -6,9 +6,6 @@ import java.awt.CardLayout;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-import javax.swing.JMenuBar;
-import javax.swing.JMenu;
-import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
 
 
@@ -19,24 +16,8 @@ public class MainFrame extends JFrame
 
 	private JPanel contentPane;
 	private LoginPanel loginPanel;
-
-	private JMenuBar menuBar;
-	private JMenu mnFile;
-	private JMenu mnHelp;
 	private TabsPane tabbedPane;
 	//private StockBrowserPanel stockBrowsingPanel;
-	private JMenu mnManageStock;
-	private JMenuItem mntmLogOut;
-	private JMenuItem mntmExit;
-	private JMenuItem mntmCorrectStockLevel;
-	private JMenuItem mntmMarkIncorrectItem;
-	private JMenu mnManageAccount;
-	private JMenuItem mntmAddNewAccount;
-	private JMenuItem mntmChangeUsername;
-	private JMenuItem mntmChangePassword;
-	private JMenuItem mntmHelp;
-	private JMenuItem mntmAbout;
-	private JMenuItem mntmAddNewItem;
 
 
 
@@ -47,50 +28,7 @@ public class MainFrame extends JFrame
 		setBounds(100, 100, 650, 460);
 		setSize(1250, 700);
 		setVisible(true);
-		menuBar = new JMenuBar();
-		setJMenuBar(menuBar);
 		
-		mnFile = new JMenu("File");
-		menuBar.add(mnFile);
-		
-		mntmLogOut = new JMenuItem("Log Out...");
-		mnFile.add(mntmLogOut);
-		
-		mntmExit = new JMenuItem("Exit");
-		mnFile.add(mntmExit);
-		
-		mnManageStock = new JMenu("Manage Stock");
-		menuBar.add(mnManageStock);
-		
-		mntmCorrectStockLevel = new JMenuItem("Correct Stock Level");
-		mnManageStock.add(mntmCorrectStockLevel);
-		
-		mntmMarkIncorrectItem = new JMenuItem("Mark Incorrect Item");
-		mnManageStock.add(mntmMarkIncorrectItem);
-		
-		mntmAddNewItem = new JMenuItem("Add New Item");
-		mnManageStock.add(mntmAddNewItem);
-		
-		mnManageAccount = new JMenu("Manage Account");
-		menuBar.add(mnManageAccount);
-		
-		mntmAddNewAccount = new JMenuItem("Add New Account");
-		mnManageAccount.add(mntmAddNewAccount);
-		
-		mntmChangeUsername = new JMenuItem("Change Username");
-		mnManageAccount.add(mntmChangeUsername);
-		
-		mntmChangePassword = new JMenuItem("Change Password");
-		mnManageAccount.add(mntmChangePassword);
-		
-		mnHelp = new JMenu("Help");
-		menuBar.add(mnHelp);
-		
-		mntmHelp = new JMenuItem("Help");
-		mnHelp.add(mntmHelp);
-		
-		mntmAbout = new JMenuItem("About");
-		mnHelp.add(mntmAbout);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new CardLayout());
